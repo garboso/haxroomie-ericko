bed98e3981096c25a497355d711bf6ac5c9e551b
christopher augusto <christopher@cromaqui.com>
2020-07-14 00:30:18 -0300

    add globals and change indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,34 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es2020': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 11
-    },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2020': true
+  },
+  'globals': {
+    '__dirname': 'readonly',
+    'process': 'readonly',
+    'HBInit': 'readonly'
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 11
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
 };
