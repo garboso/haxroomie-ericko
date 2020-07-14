6b7a676c9077b58f5454a3c8cddc30b668963fee
christopher augusto <christopher@cromaqui.com>
2020-07-14 00:29:16 -0300

    add resetColors command and fix indentation

--- plugins/chr/colors.js
+++ plugins/chr/colors.js
@@ -1,63 +1,99 @@
 /**
  * Colors plugin, admins can choose specific colors (or kits) to the teams.
  */
-
-
 const room = HBInit();
 
 room.pluginSpec = {
-    name: 'chr/colors',
-    author: 'garboso',
-    version: '1.0.0',
-    dependencies: [
-        'sav/core',
-        'sav/commands'
-    ]
+  name: 'chr/colors',
+  author: 'garboso',
+  version: '1.0.0',
+  dependencies: [
+    'sav/core',
+    'sav/commands'
+  ]
 };
 
 const colorsList = [
-    { name: 'Lion Heart',
-        type: 1,
-        options: {
-            angle: 115,
-            textColor: 0xFFFFFF,
-            colors: [0x000000, 0xFF54D7, 0xFF54D7]
-        }
-    },
-    { name: 'Lion Heart',
-        type: 2,
-        options: {
-            angle: 90,
-            textColor: 0xFFFFFF,
-            colors: [0x000000, 0x000000, 0xFF54D7]
-        }
+  {
+    name: 'Lion Heart',
+    type: 1,
+    options: {
+      angle: 115,
+      textColor: 0xFFFFFF,
+      colors: [0x000000, 0xFF54D7, 0xFF54D7]
     }
+  },
+  {
+    name: 'Lion Heart',
+    type: 2,
+    options: {
+      angle: 90,
+      textColor: 0xFFFFFF,
+      colors: [0x333333, 0x333333, 0xFF54D7]
+    }
+  },
+  {
+    name: 'Boca Juniors',
+    type: 1,
+    options: {
+      angle: 90,
+      textColor: 0xFFFFFF,
+      colors: [0x000482, 0xFFE70D, 0x230085]
+    }
+  },
+  {
+    name: 'River Plate',
+    type: 1,
+    options: {
+      angle: 55,
+      textColor: 0x0A0A0A,
+      colors: [0xFFFDFC, 0xFF0A0A, 0xFAF7F7]
+    }
+  },
+  {
+    name: 'River Plate',
+    type: 2,
+    options: {
+      angle: 145,
+      textColor: 0xFFFFFF,
+      colors: [0x000000, 0xFF1900, 0x000000]
+    }
+  }
 ];
 
 const RED_ID = 1,
-    BLUE_ID = 2;
+  BLUE_ID = 2;
 
 function getColorsList() {
-    return colorsList.map((color) => `${color.name} - ${color.type}`);
+  return colorsList.map((color) => `${color.name} - ${color.type}`);
+}
+
+function resetColors(player) {
+  if (!isAdmin(player)) return;
+
+  room.setTeamColors(RED_ID, 0, 0xFFFFFF, [0xE56E56]);
+  room.setTeamColors(BLUE_ID, 0, 0xFFFFFF, [0x5689E5]);
+
+  room.sendAnnouncement('Default colors selected.', player.id);
 }
 
 function listAllColors(player) {
-    const colorsListMessage = getColorsList().sort().join('\n');
-    room.sendAnnouncement(colorsListMessage, player.id);
+  const colorsListMessage = getColorsList().sort().join('\n');
+  room.sendAnnouncement(colorsListMessage, player.id);
 }
 
-function setColor(teamId, colorName, colorType) {
-    const colorSelected = colorsList.filter((color) => color.name === colorName && color.type === colorType)[0];
+function setColors(teamId, colorName, colorType) {
+  const colorSelected = colorsList.filter((color) => color.name === colorName && color.type === colorType)[0];
 
-    if (colorSelected != null) {
-        room.setTeamColors(teamId,
-            colorSelected.options.angle,
-            colorSelected.options.textColor,
-            colorSelected.options.colors);
-        return true;
-    } else {
-        return false;
-    }
+  if (colorSelected != null) {
+    room.setTeamColors(teamId,
+      colorSelected.options.angle,
+      colorSelected.options.textColor,
+      colorSelected.options.colors);
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isAdmin(player) {
@@ -68,40 +104,45 @@ function isAdmin(player) {
   return true;
 }
 
-function setColorCommand(teamId, player, args) {
-    if (!isAdmin(player)) return;
+function setColorsCommand(teamId, player, args) {
+  if (!isAdmin(player)) return;
 
-    const fullName = args.join(' ').split(' - ');
+  const fullName = args.join(' ').split(' - ');
 
-    const colorName = fullName[0],
-          colorType = fullName[1];
+  const colorName = fullName[0],
+    colorType = fullName[1];
 
-    console.warn(`args: ${args}`);
-    if (!setColor(teamId, colorName, parseInt(colorType))) {
-        room.sendAnnouncement('Color not found.', player.id);
-    } else {
-        room.sendAnnouncement('Nice kit.', player.id);
-    }
+  if (!setColors(teamId, colorName, parseInt(colorType))) {
+    room.sendAnnouncement('Color not found.', player.id);
+  } else {
+    room.sendAnnouncement('Nice kit.', player.id);
+  }
 }
 
 room.onCommand_listColors = {
-    function: (player) => {
-        listAllColors(player);
-    }
+  function: (player) => {
+    listAllColors(player);
+  }
 };
 
-room.onCommand_setColorRed = {
-    function: (player, args) => {
-        setColorCommand(RED_ID, player, args);
-    }
+room.onCommand_setColorsRed = {
+  function: (player, args) => {
+    setColorsCommand(RED_ID, player, args);
+  }
 };
 
-room.onCommand_setColorBlue = {
-    function: (player, args) => {
-        setColorCommand(BLUE_ID, player, args);
-    }
+room.onCommand_setColorsBlue = {
+  function: (player, args) => {
+    setColorsCommand(BLUE_ID, player, args);
+  }
+};
+
+room.onCommand_resetColors = {
+  function: (player) => {
+    resetColors(player);
+  }
 };
 
 room.onRoomLink = () => {
-    room.listAllColors = listAllColors;
+  room.listColors = listAllColors;
 };
