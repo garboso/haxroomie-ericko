7486b4473095e26c39f71a85d41ca3d1cc223d73
christopher <christopher@cromaqui.com>
2020-12-10 17:02:50 -0300

    add new championship kits

--- plugins/chr/colors.js
+++ plugins/chr/colors.js
@@ -21,226 +21,226 @@ let colorsStreakEnabled = room.pluginSpec.config.colorsStreak,
   winStreak = 0;
 
 const colorsList = [
-{
-  name: 'Lion Heart',
-  type: 1,
-  code: 'LH1',
-  options: {
-    angle: 115,
-    textColor: 0xFFFFFF,
-    colors: [0x000000, 0xFF54D7, 0xFF54D7]
+  {
+    name: 'Lion Heart',
+    type: 1,
+    code: 'LH1',
+    options: {
+      angle: 90,
+      textColor: 0x40353D,
+      colors: [0xF582D8, 0xE87BCC, 0xF582D8]
+    }
+  },
+  {
+    name: 'Lion Heart',
+    type: 2,
+    code: 'LH2',
+    options: {
+      angle: 0,
+      textColor: 0xFDE311,
+      colors: [0x212121, 0x333333, 0x212121]
+    }
+  },
+  {
+    name: 'Mistos',
+    type: 1,
+    code: 'MI1',
+    options: {
+      angle: 90,
+      textColor: 0x000000,
+      colors: [0xFECE85, 0xFD8087, 0xFECE85]
+    }
+  },
+  {
+    name: 'Mistos',
+    type: 2,
+    code: 'MI2',
+    options: {
+      angle: 90,
+      textColor: 0xFECE85,
+      colors: [0x2AA4C9, 0xFF0000, 0x2AA4C9]
+    }
+  },
+  {
+    name: 'Armandinho',
+    type: 1,
+    code: 'AR1',
+    options: {
+      angle: 34,
+      textColor: 0xFFF7FD,
+      colors: [0x0080FF, 0xCCC8CA, 0x0F8C24]
+    }
+  },
+  {
+    name: 'Armandinho',
+    type: 2,
+    code: 'AR2',
+    options: {
+      angle: 90,
+      textColor: 0x30F1FF,
+      colors: [0xA86A19, 0xE0B95E]
+    }
+  },
+  {
+    name: 'Afogados',
+    type: 1,
+    code: 'AF1',
+    options: {
+      angle: 45,
+      textColor: 0xD4D6D6,
+      colors: [0xD4D6D6, 0x004077, 0xD4D6D6]
+    }
+  },
+  {
+    name: 'Afogados',
+    type: 2,
+    code: 'AF2',
+    options: {
+      angle: 180,
+      textColor: 0x004077,
+      colors: [0xD4D6D6, 0xD4D6D6, 0xD4D6D6]
+    }
+  },
+  {
+    name: 'Barba de Itaboraí',
+    type: 1,
+    code: 'BA1',
+    options: {
+      angle: 60,
+      textColor: 0xFFFFFF,
+      colors: [0xFF8C00, 0xFF8C00, 0x000080]
+    }
+  },
+  {
+    name: 'Barba de Itaboraí',
+    type: 2,
+    code: 'BA2',
+    options: {
+      angle: 0,
+      textColor: 0xFFFFFF,
+      colors: [0x993399]
+    }
+  },
+  {
+    name: 'Dribles Sacanas',
+    type: 1,
+    code: 'DS1',
+    options: {
+      angle: 231,
+      textColor: 0xFFFFFF,
+      colors: [0x000000, 0x9E1313, 0x000000]
+    }
+  },
+  {
+    name: 'Marra Pura',
+    type: 1,
+    code: 'MP1',
+    options: {
+      angle: 90,
+      textColor: 0xD1BE17,
+      colors: [0xFCE400, 0xFFFFFF, 0xFFFFFF]
+    }
+  },
+  {
+    name: 'Marra Pura',
+    type: 2,
+    code: 'MP2',
+    options: {
+      angle: 90,
+      textColor: 0xD1BE17,
+      colors: [0xFCE400, 0x000000, 0x000000]
+    }
+  },
+  {
+    name: 'Pó Pó Pó',
+    type: 1,
+    code: 'PO1',
+    options: {
+      angle: 0,
+      textColor: 0xF2C37D,
+      colors: [0xF2C37D]
+    }
+  },
+  {
+    name: 'Pó Pó Pó',
+    type: 2,
+    code: 'PO2',
+    options: {
+      angle: 45,
+      textColor: 0x000000,
+      colors: [0x000000, 0xEF0D98, 0x000000]
+    }
+  },
+  {
+    name: 'Vitória do Bahia',
+    type: 1,
+    code: 'VB1',
+    options: {
+      angle: 60,
+      textColor: 0xFFFFFF,
+      colors: [0x000000, 0xFF0000, 0x0016E0]
+    }
+  },
+  {
+    name: 'Vitória do Bahia',
+    type: 2,
+    code: 'VB2',
+    options: {
+      angle: 60,
+      textColor: 0xFFFFFF,
+      colors: [0x00A60B, 0x6B93FF, 0xE00000]
+    }
+  },
+  {
+    name: 'Loukos',
+    type: 1,
+    code: 'LO1',
+    options: {
+      angle: 0,
+      textColor: 0xFFFFFF,
+      colors: [0xFFFFFF, 0xC40A1C, 0xFFFFFF]
+    }
+  },
+  {
+    name: 'Loukos',
+    type: 2,
+    code: 'LO2',
+    options: {
+      angle: 60,
+      textColor: 0xD1B43F,
+      colors: [0x151269]
+    }
+  },
+  {
+    name: 'Vendo Monza',
+    type: 1,
+    code: 'VM1',
+    options: {
+      angle: 0,
+      textColor: 0xFCBF49,
+      colors: [0x75AADB]
+    }
+  },
+  {
+    name: 'Vendo Monza',
+    type: 2,
+    code: 'VM2',
+    options: {
+      angle: 0,
+      textColor: 0xFCBF49,
+      colors: [0x75AADB]
+    }
+  },
+  {
+    name: 'Bomba de Savvoya',
+    type: 1,
+    code: 'BS1',
+    options: {
+      angle: 90,
+      textColor: 0xDE0000,
+      colors: [0xCFA930, 0xFFFFFF, 0xFFFFFF]
+    }
   }
-},
-{
-  name: 'Lion Heart',
-  type: 2,
-  code: 'LH2',
-  options: {
-    angle: 90,
-    textColor: 0xFFFFFF,
-    colors: [0x333333, 0x333333, 0xFF54D7]
-  }
-},
-{
-  name: 'Mistos',
-  type: 1,
-  code: 'MI1',
-  options: {
-    angle: 0,
-    textColor: 0x309111,
-    colors: [0xDE0000, 0xDE0000, 0xFFFFFF]
-  }
-},
-{
-  name: 'Mistos',
-  type: 2,
-  code: 'MI2',
-  options: {
-    angle: 0,
-    textColor: 0xDE0000,
-    colors: [0xFFFFFF, 0x309111, 0x309111]
-  }
-},
-{
-  name: 'Armandinho',
-  type: 1,
-  code: 'AR1',
-  options: {
-    angle: 34,
-    textColor: 0xFFF7FD,
-    colors: [0x0080FF, 0xCCC8CA, 0x0F8C24]
-  }
-},
-{
-  name: 'Armandinho',
-  type: 2,
-  code: 'AR2',
-  options: {
-    angle: 90,
-    textColor: 0x30F1FF,
-    colors: [0xA86A19, 0xE0B95E]
-  }
-},
-{
-  name: 'Afogados',
-  type: 1,
-  code: 'AF1',
-  options: {
-    angle: 45,
-    textColor: 0xD4D6D6,
-    colors: [0xD4D6D6, 0x004077, 0xD4D6D6]
-  }
-},
-{
-  name: 'Afogados',
-  type: 2,
-  code: 'AF2',
-  options: {
-    angle: 180,
-    textColor: 0x004077,
-    colors: [0xD4D6D6, 0xD4D6D6, 0xD4D6D6]
-  }
-},
-{
-  name: 'Barba Flu',
-  type: 1,
-  code: 'BF1',
-  options: {
-    angle: 65,
-    textColor: 0xFFFFFF,
-    colors: [0xFF8000, 0xFF8000, 0x000000]
-  }
-},
-{
-  name: 'Barba Flu',
-  type: 2,
-  code: 'BF2',
-  options: {
-    angle: 0,
-    textColor: 0xFFFFFF,
-    colors: [0x107500, 0x940000, 0x107500]
-  }
-},
-{
-  name: 'Dribles Sacanas',
-  type: 1,
-  code: 'DS1',
-  options: {
-    angle: 231,
-    textColor: 0xFFFFFF,
-    colors: [0x000000, 0x9E1313, 0x000000]
-  }
-},
-{
-  name: 'Marra Pura',
-  type: 1,
-  code: 'MP1',
-  options: {
-    angle: 90,
-    textColor: 0xD1BE17,
-    colors: [0xFCE400, 0xFFFFFF, 0xFFFFFF]
-  }
-},
-{
-  name: 'Marra Pura',
-  type: 2,
-  code: 'MP2',
-  options: {
-    angle: 90,
-    textColor: 0xD1BE17,
-    colors: [0xFCE400, 0x000000, 0x000000]
-  }
-},
-{
-  name: 'Pó Pó Pó',
-  type: 1,
-  code: 'PO1',
-  options: {
-    angle: -60,
-    textColor: 0x000000,
-    colors: [0xFFFFFF, 0xFF00AA, 0xFFFFFF]
-  }
-},
-{
-  name: 'Pó Pó Pó',
-  type: 2,
-  code: 'PO2',
-  options: {
-    angle: -60,
-    textColor: 0xFFFFFF,
-    colors: [0x0E0C00, 0x0E0C00, 0xFF00AA]
-  }
-},
-{
-  name: 'Vitória do Bahia',
-  type: 1,
-  code: 'VB1',
-  options: {
-    angle: 60,
-    textColor: 0xFFFFFF,
-    colors: [0x000000, 0xFF0000, 0x0016E0]
-  }
-},
-{
-  name: 'Vitória do Bahia',
-  type: 2,
-  code: 'VB2',
-  options: {
-    angle: 60,
-    textColor: 0xFFFFFF,
-    colors: [0x00A60B, 0x6B93FF, 0xE00000]
-  }
-},
-{
-  name: 'Loukos',
-  type: 1,
-  code: 'LO1',
-  options: {
-    angle: 0,
-    textColor: 0xFFFFFF,
-    colors: [0xFFFFFF, 0xC40A1C, 0xFFFFFF]
-  }
-},
-{
-  name: 'Loukos',
-  type: 2,
-  code: 'LO2',
-  options: {
-    angle: 1,
-    textColor: 0xFFFFFF,
-    colors: [0x00A60B, 0x6B93FF, 0xE00000]
-  }
-},
-{
-  name: 'Vendo Monza',
-  type: 1,
-  code: 'VM1',
-  options: {
-    angle: 120,
-    textColor: 0x001E30,
-    colors: [0x660000, 0x7D0000, 0x7D0000]
-  }
-},
-{
-  name: 'Vendo Monza',
-  type: 2,
-  code: 'VM2',
-  options: {
-    angle: 60,
-    textColor: 0x002033,
-    colors: [0xFFFFFF, 0xFF3030, 0xFFFFFF]
-  }
-},
-{
-  name: 'Bomba de Savvoya',
-  type: 1,
-  code: 'BS1',
-  options: {
-    angle: 0,
-    textColor: 0x1100FF,
-    colors: [0x0E8700, 0xFFFFFF, 0xE80F00]
-  }
-}
 ];
 
 const RED_ID = 1,
@@ -293,11 +293,7 @@ function isAdmin(player) {
 }
 
 function setColorsCommand(teamId, player, args) {
-  const fullName = args.join(' ').split(' - '),
-    colorName = fullName[0],
-    colorType = fullName[1];
-
-  if (!setColors(teamId, colorName, parseInt(colorType))) {
+  if (!setColors(teamId, args[0])) {
     room.sendAnnouncement('Uniforme não encontrado.', player.id, 0xCF3838, 'small-bold');
   } else {
     room.sendAnnouncement(`Bonita camisa, ${teamId === RED_ID ? 'vermelhinho' : 'azulzinho'}.`, player.id, 0x69C969, 'small-bold');
@@ -459,13 +455,13 @@ room.onCommand_desabilitar_uniformes_invencibilidade = {
   }
 };
 
-room.onCommand_definir_uniforme_red = {
+room.onCommand_definir_uniforme_vermelho = {
   function: (player, args) => {
     setColorsCommand(RED_ID, player, args);
   }
 };
 
-room.onCommand_definir_uniforme_blue = {
+room.onCommand_definir_uniforme_azul = {
   function: (player, args) => {
     setColorsCommand(BLUE_ID, player, args);
   }
