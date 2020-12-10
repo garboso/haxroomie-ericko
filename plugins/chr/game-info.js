661aaa4a0c863ccb002b0ffe987057f38ea60501
christopher <christopher@cromaqui.com>
2020-12-10 17:11:00 -0300

    add new plugins

---
+++ plugins/chr/game-info.js
@@ -0,0 +1,191 @@
+const room = HBInit();
+
+room.pluginSpec = {
+  name: `chr/game-info`,
+  author: `christopher`,
+  version: `1.0.0`,
+  dependencies: [
+    `sav/core`
+  ],
+  config: {
+    championshipMode: false
+  }
+};
+
+const RED_ID = 1,
+  BLUE_ID = 2,
+  ERICKO_TEAMS = [
+    { name: 'Afogados', acronym: 'AFO', icon: '🏊', custom: true },
+    { name: 'Barba de Itaboraí', acronym: 'BAR', icon: '🧔', custom: true },
+    { name: 'Bomba de Savvoya', acronym: 'BDS', icon: '💣', custom: true },
+    { name: 'Dribles Sacanas', acronym: 'DRI', icon: '✍️', custom: true },
+    { name: 'Lion Heart', acronym: 'LIO', icon: '🔥', custom: true },
+    { name: 'Loukos', acronym: 'LOU',  icon: '🏆', custom: true },
+    { name: 'Mistos', acronym: 'MIS', icon: '🐊', custom: true },
+    { name: 'Pó Pó Pó', acronym: 'POP', icon: '👃', custom: true },
+    { name: 'Vendo Monza', acronym: 'VEM', icon: '🚗', custom: true },
+    { name: 'Vitória do Bahia', acronym: 'VIT', icon: '🏖️', custom: true }];
+
+let fixture = null,
+  teamsInfos = {
+    1: { name: 'Vermelho', acronym: 'VER', icon: '🔴', custom: false },
+    2: { name: 'Azul', acronym: 'AZU', icon: '🔵', custom: false } };
+
+function setFixture(player, args) {
+  if (/^[0-9]*$/.test(args[0]) && parseInt(args[0]) >= 0) {
+    fixture = parseInt(args[0]);
+
+    if (fixture === 0) {
+      room.sendAnnouncement(`👍 O jogo será registrado como amistoso.`, player.id, 0x69C969, 'small-bold');
+    } else {
+      room.sendAnnouncement(`👍 O jogo será registrado como pertencente a ${fixture}ª rodada.`, player.id, 0x69C969, 'small-bold');
+    }
+  } else {
+    room.sendAnnouncement(`⚠️ O valor inserido não foi aceito. Por favor, insira um número correspondente a rodada, 0 para partida amistosa.`, player.id, 0xffbf00, 'small-bold');
+  }
+}
+
+function listAllTeams(player) {
+  let message = '';
+
+  ERICKO_TEAMS.forEach(team => message += `${team.name} (${team.acronym}), ` );
+
+  room.sendAnnouncement(message.slice(0, -2), player.id, 0x69C969, 'small-bold');
+}
+
+function showMatchInfo(player) {
+  let message = '\n';
+
+  message += `${(fixture != null ? (fixture === 0 ? `Amistoso` : `Válido pela ${fixture}ª rodada:\n`) : `⚠️ Rodada não definida.`)}\n`;
+  message += `${getTeamInfo(RED_ID)} - ${getTeamInfo(BLUE_ID)}`;
+
+  room.sendAnnouncement(message, player.id, 0x4094FF, 'small-bold');
+}
+
+function getMatchInfo() {
+  return { fixture: fixture, teams: getTeams() };
+}
+
+function getTeamInfo(teamId) {
+  return `${teamsInfos[teamId].icon} ${teamsInfos[teamId].name} (${teamsInfos[teamId].acronym})`;
+}
+
+function getTeamNames() {
+  return { 1: teamsInfos[RED_ID].name, 2: teamsInfos[BLUE_ID].name };
+}
+
+function checkTeamNames() {
+  for (let key in teamsInfos) {
+    if (!teamsInfos[key].custom) return false;
+  }
+
+  return true;
+}
+
+function setTeam(player, args, teamId) {
+  if (/^[A-Z]{3}$/.test(args[0])) {
+    const team = ERICKO_TEAMS.find(team => team.acronym === args[0]);
+
+    if (team !== undefined) {
+      teamsInfos[teamId] = team;
+      room.sendAnnouncement(`👍 ${team.name} escolhido com sucesso.`, player.id, 0x69C969, 'small-bold');
+      getMatchInfo(player);
+    } else {
+      room.sendAnnouncement('⚠️ Time não encontrado. Veja novamente a lista de times disponíveis abaixo:', player.id, 0xffbf00, 'small-bold');
+      listAllTeams(player);
+    }
+  } else {
+    room.sendAnnouncement('⚠️ O valor inserido não foi aceito. Por favor, insira a sigla com três letras referente ao time escolhido.', player.id, 0xffbf00, 'small-bold');
+    listAllTeams(player);
+  }
+}
+
+function resetTeamsInfos() {
+  teamsInfos = {
+    1: { name: 'Vermelho', acronym: 'VER', icon: '🔴', custom: false },
+    2: { name: 'Azul', acronym: 'AZU', icon: '🔵', custom: false } };
+  fixture = null;
+}
+
+function getTeams() {
+  const players = room.getPlayerList().filter(p => p.team !== 0);
+  let red, blue;
+
+  red = teamsInfos[RED_ID];
+  red.players = players.filter(p => p.team === RED_ID);
+  blue = teamsInfos[BLUE_ID];
+  blue.players = players.filter(p => p.team === BLUE_ID);
+
+  return {
+    [RED_ID]: red,
+    [BLUE_ID]: blue };
+}
+
+function isAdmin(player) {
+  if (!player.admin) {
+    room.sendAnnouncement('❌ Você não possui privilégios para usar esse comando.', player.id, 0xCF3838, 'small-bold');
+    return false;
+  }
+  return true;
+}
+
+room.getTeamNames = getTeamNames;
+room.getTeams = getTeams;
+room.getMatchInfo = getMatchInfo;
+
+room.onGameStart = (player) => {
+  if (room.pluginSpec.config.championshipMode) {
+    if (fixture === null) {
+      room.stopGame();
+      room.sendAnnouncement(`Para iniciar o jogo, informe por qual rodada será disputado o jogo através do comando '!definir rodada <número da rodada>'.`, player.id, 0xffbf00, 'small-bold');
+    } else if (!checkTeamNames()) {
+      room.stopGame();
+      room.sendAnnouncement(`Para iniciar o jogo, selecione os times que irão jogar utilizando o comando !definir time vermelho <sigla> ou !definir time azul <sigla>`, player.id, 0xffbf00, 'small-bold');
+      listAllTeams(player);
+    }
+  }
+};
+
+room.onTeamVictory = () => {
+  if (room.pluginSpec.config.championshipMode) {
+    fixture = null;
+  }
+};
+
+room.onGameStop = () => {
+  resetTeamsInfos();
+};
+
+room.onCommand_definir_rodada = {
+  function: (player, args) => {
+    if (!isAdmin(player)) return;
+    setFixture(player, args);
+  }
+};
+
+room.onCommand_definir_time_vermelho = {
+  function: (player, args) => {
+    if (!isAdmin(player)) return;
+    setTeam(player, args, RED_ID);
+  }
+};
+
+room.onCommand_definir_time_azul = {
+  function: (player, args) => {
+    if (!isAdmin(player)) return;
+    setTeam(player, args, BLUE_ID);
+  }
+};
+
+room.onCommand_listar_times = {
+  function: (player) => {
+    if (!isAdmin(player)) return;
+    listAllTeams(player);
+  }
+};
+
+room.onCommand_ver_informações_jogo = {
+  function: (player) => {
+    showMatchInfo(player);
+  }
+};
---
+++ plugins/chr/narrator.js
@@ -0,0 +1,162 @@
+/**
+ * Plugin that generate phrases during the game.
+ */
+const room = HBInit();
+
+room.pluginSpec = {
+  name: `chr/narrator`,
+  author: `christopher`,
+  version: `1.0.0`,
+  dependencies: [
+    `sav/core`,
+    `sav/cron`,
+    `chr/game-info`,
+    `chr/fake-soccer-news`,
+    `chr/stats`
+  ]
+};
+
+let gameRunning = false,
+  goalScored = false,
+  gamePaused = false,
+  teams;
+
+const gameInfo = room.getPlugin(`chr/game-info`),
+  news = room.getPlugin(`chr/fake-soccer-news`),
+  stats = room.getPlugin(`chr/stats`);
+
+function sendKickOffPhrase () {
+  const messages = ['Rolou o melão!',
+    'Acerta o seu daí que eu arredondo o meu daqui, eeeessstá valendo!',
+    'Bola em jogo pra você ligado no Hax!',
+    `Autoriza o árbitro, mexe na bola o ${teams[1].name}!`,
+    'Bola rolando!'];
+
+  let message = `${messages[Math.floor(Math.random() * messages.length)].toUpperCase()}`;
+
+  room.sendAnnouncement(message, null, 0xEBEBEB, 'small-italic');
+}
+
+function sendOpeningPhrase () {
+  let message = `Os times já estão alinhados em campo e prontos para o início da partida.\n`;
+
+  const matchInfo = gameInfo.getMatchInfo();
+  teams = matchInfo.teams;
+
+  message += `${(matchInfo.fixture !== null ? (matchInfo.fixture === 0 ? `Amistoso` : `${matchInfo.fixture}ª rodada`) : ``)} \n\n`;
+  message += `${teams[1].icon} ${teams[1].name} - ${teams[2].name} ${teams[2].icon} \n\n`;
+
+  for (let id in teams) {
+    message += `${teams[id].name} ${teams[id].icon}\n    `;
+
+    if (teams[id].players !== undefined) {
+      teams[id].players.forEach(player => message += `${player.name}, `);
+    }
+
+    message = message.slice(0, -2);
+    message += `\n`;
+  }
+
+  room.sendAnnouncement(message, null, 0xEBEBEB, 'small-italic');
+}
+
+function sendGoalPhrase() {
+  const goals = stats.getGoals(),
+    goal = goals[goals.length - 1];
+
+  const messages = [`Éééééééééé do ${getGoalTeam(goal).name}, ${goal.isOwnGoal ? `${goal.scorer.name} jogou contra a própria meta!` : `${goal.scorer.name}!!!` }\nEram jogados ${getFormattedGoalTime(goal.time)} e foi lá, balançou o capim no fundo do gol!`];
+
+  let message = `⚽ ${messages[Math.floor(Math.random() * messages.length)].toUpperCase()}`;
+
+  room.sendAnnouncement(message, null, 0xEBEBEB, 'small-italic');
+}
+
+function getGoalTeam(goal) {
+  if (goal.isOwnGoal) {
+    if (goal.scorer.team === 1) return teams[2];
+    if (goal.scorer.team === 2) return teams[1];
+  } else {
+    return teams[goal.scorer.team];
+  }
+}
+
+function sendPausePhrase() {
+  const messages = ['Pare!',
+    'Tá lá o corpo estendido no chão!',
+    'Pediu pra parar, parou!'];
+
+  let message = `${messages[Math.floor(Math.random() * messages.length)].toUpperCase()}`;
+
+  room.sendAnnouncement(message, null, 0xEBEBEB, 'small-italic');
+}
+
+function sendPossessionTeamsPhrase () {
+  const percentage = stats.getPossessionPerTeam();
+  let message = `\n\nPosse de Bola\n\n`.toUpperCase();
+
+  teams = gameInfo.getTeams();
+  message += `   ${teams[1].icon}  ${percentage[1]}%\n   ${teams[2].icon}  ${percentage[2]}%`;
+
+  room.sendAnnouncement(message, null, 0xEBEBEB, 'small-italic');
+}
+
+function getFormattedGoalTime(time) {
+  const minutes = Math.floor(time / 60),
+    seconds = time % 60;
+
+  return `${addPadZeroIfNecessary(minutes)}:${addPadZeroIfNecessary(seconds)}`;
+}
+
+function addPadZeroIfNecessary(unit) {
+  if (unit === 0 || unit <= 9) {
+    return `0${unit}`;
+  }
+
+  return unit;
+}
+
+room.onGameTick = () => {
+  if (!gameRunning) {
+    if ((room.getBallPosition().x !== 0 || room.getBallPosition().y !== 0) && !goalScored) {
+      gameRunning = true;
+      sendKickOffPhrase();
+    } else {
+      return;
+    }
+  }
+};
+
+room.onGameStop = () => {
+  gameRunning = false;
+  goalScored = false;
+};
+
+room.onGamePause = () => {
+  gamePaused = true;
+  sendPausePhrase();
+};
+
+room.onGameUnpause = () => {
+  gamePaused = false;
+};
+
+room.onGameStart = () => {
+  sendOpeningPhrase();
+};
+
+room.onTeamGoal = () => {
+  goalScored = true;
+  sendGoalPhrase();
+};
+
+room.onCron45GameSeconds = () => {
+  if (gamePaused || !gameRunning) return;
+
+  sendPossessionTeamsPhrase();
+};
+
+room.onCron120GameSeconds = () => {
+  if (gamePaused || !gameRunning) return;
+
+  room.sendAnnouncement(news.getOneTransfer(), null, 0xEBEBEB, 'small-italic');
+};
